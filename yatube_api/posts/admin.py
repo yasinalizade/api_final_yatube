8f2439e6714947f089ddcd9d1ed4d8608c80c38e
Yasin Alizade <94525961+yasinalizade@users.noreply.github.com>
2022-06-01 11:10:44 +0300

    Update admin.py

--- yatube_api/posts/admin.py
+++ yatube_api/posts/admin.py
@@ -2,7 +2,53 @@ from django.contrib import admin
 
 from .models import Comment, Follow, Group, Post
 
-admin.site.register(Post)
-admin.site.register(Group)
-admin.site.register(Comment)
-admin.site.register(Follow)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'text',
+        'pub_date',
+        'author',
+        'group',
+        'image',
+    )
+    list_editable = ('group',)
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'title',
+        'slug',
+        'description'
+    )
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'text',
+        'post',
+        'author',
+        'created'
+    )
+    search_fields = ('text',)
+    list_filter = ('created',)
+
+
+class FollowAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'following',
+        'user',
+    )
+
+
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Follow, FollowAdmin)
+admin.site.register(Group, GroupAdmin)
+admin.site.register(Post, PostAdmin)
